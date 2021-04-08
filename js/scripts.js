75565e514acb1e3fa8bb0a5bc780771b6cfd883f
icld <ian@icldesign.com>
2021-04-08 09:48:14 -0700

    refactor

---
+++ js/scripts.js
@@ -0,0 +1,109 @@
+const body = document.querySelector('body')
+const h1 = document.querySelector('h1')
+h1.classList.add('ready')
+setTimeout(() => h1.classList.remove('ready'), 1000)
+const modalContainer = document.createElement('div')
+body.appendChild(modalContainer)
+const gallery = document.querySelector('#gallery')
+let profileData = []
+
+
+
+
+// SEARCH
+const searchContainer = document.querySelector('.search-container')
+const searchHTML = `<form action="#" method="get">
+<input type="search" id="search-input" class="search-input" placeholder="Search...">
+<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+</form>`
+searchContainer.insertAdjacentHTML('beforeend', searchHTML)
+
+
+//FETCH
+fetch('https://randomuser.me/api/?page=1&results=12&seed=abc')
+    .then(checkStatus)
+    .then(response => response.json())
+    .then(data => {
+        profileData = data.results
+        console.log(profileData);
+        generateCard(profileData);
+        generateModal(profileData);
+    })
+    .catch(error => console.log('Looks like there was a problem', error))
+
+function checkStatus(response) {
+    if (response.ok) {
+        return Promise.resolve(response);
+    } else {
+        return Promise.reject(new Error(response.statusText))
+    }
+}
+
+function generateCard(data) {
+    data.forEach(profile => {
+        const galleryHTML = `<div class="card">
+<div class="card-img-container">
+    <img class="card-img" src="${profile.picture.medium}" alt="profile picture">
+</div>
+<div class="card-info-container">
+    <h3 id="name" class="card-name cap">${profile.name.first} ${profile.name.last}</h3>
+    <p class="card-text">${profile.email}</p>
+    <p class="card-text cap">${profile.location.city}, ${profile.location.state}</p>
+</div>
+</div>`;
+        gallery.insertAdjacentHTML('beforeend', galleryHTML);
+        // const cards = document.querySelectorAll('.card')
+        // cards.addEventListener('click', e => {
+        //     console.log('fart')
+        // })
+    });
+
+}
+
+gallery.addEventListener('click', e => {
+
+    if (e.target.classList.contains('card')) {
+        console.log('fart')
+    }
+})
+
+// cards.forEach(card => {
+//     card.addEventListener('click', (e) => {
+//         console.log(e.target)
+//     })
+// })
+
+function generateModal(data) {
+    data.forEach(profile => {
+        const modalHTML = `<div class="modal-container" style="display: none;">
+<div class="modal">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    <div class="modal-info-container">
+        <img class="modal-img" src="${profile.picture.large}" alt="profile picture">
+        <h3 id="name" class="modal-name cap">}</h3>
+        <p class="modal-text">email</p>
+        <p class="modal-text cap">city</p>
+        <hr>
+        <p class="modal-text">(555) 555-5555</p>
+        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+        <p class="modal-text">Birthday: 10/21/2015</p>
+    </div>
+</div>`;
+        modalContainer.insertAdjacentHTML('beforeend', modalHTML)
+    }
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
