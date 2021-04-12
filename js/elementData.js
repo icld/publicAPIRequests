267da323757a90111634b64a00da438ff162c969
icld <ian@icldesign.com>
2021-04-11 23:18:43 -0700

    clean up for review

---
+++ js/elementData.js
@@ -0,0 +1,58 @@
+/* Ian Cameron Lyles 
+ian@icldesign.com
+Treehouse Full Stack Javascript Techdegree 
+Unit 5 project 
+Public API Requests */
+
+
+const h1 = document.querySelector('h1')
+h1.classList.add('ready')
+setTimeout(() => h1.classList.remove('ready'), 1000)
+
+function galleryHTML(profile) {
+    const galleryHTML = `<div class="card">
+<div class="card-img-container">
+    <img class="card-img" src="${profile.picture.medium}" alt="profile picture">
+</div>
+<div class="card-info-container">
+    <h3 id="name" class="card-name cap">${profile.name.first} ${profile.name.last}</h3>
+    <p class="card-text">${profile.email}</p>
+    <p class="card-text cap">${profile.location.city}, ${profile.location.state}</p>
+</div>
+</div>`;
+    return galleryHTML
+}
+
+function modalHTML(data) {
+    const modalHTML = `<div class="modal-container ready">
+<div class="modal ready">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    <div class="modal-info-container">
+        <img class="modal-img" src="${data.picture.large}" alt="profile picture">
+        <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
+        <p class="modal-text">${data.email}</p>
+        <p class="modal-text cap">${data.location.city}</p>
+        <hr>
+        <p class="modal-text">${formatPhone(data.phone)}</p>
+        <p class="modal-text">${data.location.street.number} ${data.location.street.name} , ${data.location.city}, ${data.location.state} ${data.location.postcode}</p>
+        <p class="modal-text">Birthday: ${formatDOB(data.dob.date)}</p>
+    </div>
+    <div class='modal-bottom-btn-container'>
+    <button type="button" id="modal-prev-btn" class="modal-prev-btn">prev</button>
+    <button type="button" id="modal-next-btn" class="modal-next-btn">next</button>
+   </div>
+    </div>`;
+
+    return modalHTML
+}
+
+const searchHTML = `<form action="#" method="get">
+<input type="search" id="search-input" class="search-input" placeholder="Search...">
+<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+</form>`
+
+
+
+const errorHTML = `<div class="error-message">
+<strong style="font-size:100px">No Users Found &#129409</strong>
+</div>`
--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,49 @@
+/* Ian Cameron Lyles 
+ian@icldesign.com
+Treehouse Full Stack Javascript Techdegree 
+Unit 5 project 
+Public API Requests */
+
 const body = document.querySelector('body')
-const h1 = document.querySelector('h1')
-h1.classList.add('ready')
-setTimeout(() => h1.classList.remove('ready'), 1000)
 const gallery = document.querySelector('#gallery')
 let profileData = []
-let click = 0
-
-// SEARCH
 const searchContainer = document.querySelector('.search-container')
-const searchHTML = `<form action="#" method="get">
-<input type="search" id="search-input" class="search-input" placeholder="Search...">
-<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-</form>`
 searchContainer.insertAdjacentHTML('beforeend', searchHTML)
-const submit = document.querySelector('#search-submit')
 const searchInput = document.querySelector('#search-input')
 
+
+// EVENT LISTENERS
 searchContainer.addEventListener('submit', searchFilter)
 searchInput.addEventListener('keyup', searchFilter)
+body.addEventListener('click', handleModalBtns)
+gallery.addEventListener('click', handleCardClick)
+
+//FETCH
+fetch('https://randomuser.me/api/?results=12')
+    .then(checkStatus)
+    .then(response => response.json())
+    .then(data => {
+        profileData = data.results
+        console.log(profileData);
+        generateCard(profileData);
+    })
+    .catch(error => console.log('Looks like there was a problem', error))
+
+
+// FUNCTIONS
+function checkStatus(response) {
+    if (response.ok) {
+        return Promise.resolve(response);
+    } else {
+        return Promise.reject(new Error(response.statusText))
+    }
+}
+
+function generateCard(data) {
+    data.forEach(profile => {
+        gallery.insertAdjacentHTML('beforeend', galleryHTML(profile));
+    });
+}
 
 function searchFilter() {
     removeError()
@@ -28,25 +54,19 @@ function searchFilter() {
         const userInput = searchInput.value.toLowerCase()
 
         if (cardName.includes(userInput) || userInput.length === 0) {
-            console.log(`${cardName} is a match with input: ${userInput}`)
             card.style.display = ''
         } else {
             card.style.display = 'none'
             blankCard += 1
         }
         if (blankCard === 12) {
-            const html =
-                `<div class="error-message">
-                <strong style="font-size:100px">No Users Found &#129409</strong>
-            </div>`
-            console.log('all blank')
-            body.insertAdjacentHTML('beforeend', html)
+            body.insertAdjacentHTML('beforeend', errorHTML)
             h1.textContent = 'Whoopsy!'
         }
     })
 }
 
-const removeError = () => {
+function removeError() {
     if (body.textContent.includes('No Users Found')) {
         const errorMessage = document.querySelector('.error-message')
         errorMessage.remove()
@@ -54,37 +74,11 @@ const removeError = () => {
     }
 }
 
-
-function generateCard(data) {
-    data.forEach(profile => {
-        const galleryHTML = `<div class="card">
-<div class="card-img-container">
-    <img class="card-img" src="${profile.picture.medium}" alt="profile picture">
-</div>
-<div class="card-info-container">
-    <h3 id="name" class="card-name cap">${profile.name.first} ${profile.name.last}</h3>
-    <p class="card-text">${profile.email}</p>
-    <p class="card-text cap">${profile.location.city}, ${profile.location.state}</p>
-</div>
-</div>`;
-        gallery.insertAdjacentHTML('beforeend', galleryHTML);
-        const profileGrab = { profile }
-        return profileGrab
-    });
-
-}
-const galleryCards = document.getElementsByClassName("card card")
-
-gallery.addEventListener('click', e => {
+function handleCardClick(e) {
     const parent = gallery
     const card = e.target.closest('.card')
-
     if (e.target !== parent) {
-        click++
-        console.log(click)
         const userEmail = card.querySelector('.card-text').textContent
-        console.log(card)
-        console.log(userEmail)
         profileData.forEach(profile => {
             if (profile.email === userEmail) {
                 console.log(profile)
@@ -98,9 +92,9 @@ gallery.addEventListener('click', e => {
             }
         })
     }
-})
+}
 
-body.addEventListener('click', e => {
+function handleModalBtns(e) {
     if (gallery.nextElementSibling.className === 'modal-container') {
         const prev = document.getElementById('modal-prev-btn')
         const next = document.getElementById('modal-next-btn')
@@ -110,6 +104,13 @@ body.addEventListener('click', e => {
         let cardNumber;
 
         if (e.target === prev) {
+            makePrevModal()
+        }
+        else if (e.target === next) {
+            makeNextModal()
+        }
+
+        function makePrevModal() {
             for (let i = 0; i < cards.length; i++) {
                 cardNumber = i;
                 const cardName = cards[i].querySelector('#name').textContent.toLowerCase()
@@ -127,7 +128,7 @@ body.addEventListener('click', e => {
             }
         }
 
-        else if (e.target === next) {
+        function makeNextModal() {
             for (let i = 0; i < cards.length; i++) {
                 cardNumber = i;
                 const cardName = cards[i].querySelector('#name').textContent.toLowerCase()
@@ -145,8 +146,7 @@ body.addEventListener('click', e => {
             }
         }
     }
-})
-
+}
 
 function generateModal(data) {
     gallery.insertAdjacentHTML('afterend', modalHTML(data))
@@ -161,8 +161,6 @@ function showModal() {
     container.style.display = 'block'
     setTimeout(() => modal.classList.remove('ready'), 200)
     setTimeout(() => container.classList.remove('ready'), 200)
-
-
 }
 
 function modalRemove() {
@@ -176,42 +174,13 @@ function modalRemove() {
     })
 }
 
-
-function modalHTML(data) {
-    const modalHTML = `<div class="modal-container ready">
-<div class="modal ready">
-    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    <div class="modal-info-container">
-        <img class="modal-img" src="${data.picture.large}" alt="profile picture">
-        <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
-        <p class="modal-text">${data.email}</p>
-        <p class="modal-text cap">${data.location.city}</p>
-        <hr>
-        <p class="modal-text">${formatPhone(data.phone)}</p>
-        <p class="modal-text">${data.location.street.number} ${data.location.street.name} , ${data.location.city}, ${data.location.state} ${data.location.postcode}</p>
-        <p class="modal-text">Birthday: ${formatDOB(data.dob.date)}</p>
-    </div>
-    <div class='modal-bottom-btn-container'>
-    <button type="button" id="modal-prev-btn" class="modal-prev-btn">prev</button>
-    <button type="button" id="modal-next-btn" class="modal-next-btn">next</button>
-   </div>
-    </div>`;
-    // if (click % 2 == 0) {
-    //     const container = document.querySelector('.modal', 'ready')
-    //     console.log(container)
-    //     container.classList.remove('ready')
-    //     container.classList.add('readyDown')
-    // }
-    return modalHTML
-}
-
 function formatPhone(oldPhone) {
     const cleaned = ('' + oldPhone).replace(/\D/g, '');
     const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
     if (match) {
         return '(' + match[1] + ') ' + match[2] + '-' + match[3];
     }
-    return null;
+    // return null;
 }
 
 function formatDOB(date) {
@@ -220,24 +189,7 @@ function formatDOB(date) {
 }
 
 
-//FETCH
-fetch('https://randomuser.me/api/?results=12')
-    .then(checkStatus)
-    .then(response => response.json())
-    .then(data => {
-        profileData = data.results
-        console.log(profileData);
-        generateCard(profileData);
-    })
-    .catch(error => console.log('Looks like there was a problem', error))
 
-function checkStatus(response) {
-    if (response.ok) {
-        return Promise.resolve(response);
-    } else {
-        return Promise.reject(new Error(response.statusText))
-    }
-}
 
 
 
